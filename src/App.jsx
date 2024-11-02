cfac4b1a1bfc61cbc97f8a2a00e92b67a8ec41f4
Kamal <bishnoikamal5529@gmail.com>
2024-11-02 04:13:50 -0400

    middleIcon removed and styles adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,19 @@ import { useState } from "react"
 import  Login  from "./Components/Login"
 import Signup from "./Components/Signup"
 import "./Styles/App.css"
-import "./Styles/logosvg.css"
 import Container from "react-bootstrap/Container"
-import LogoSvg from "./Components/LogoSvg"
 
 function App() {
   const [isLoginVisible, setLoginVisible] = useState(true);
 
   return (
-    <Container className="main-container ps-lg-5 ps-2 mt-4 pt-4 pb-4 pe-5 bg-light-subtle flex-column flex-md-row justify-content-between align-items-center"> 
+    <Container className="main-container p-4 bg-light-subtle"> 
       <section className={isLoginVisible?"login-page":"login-page login-hide"}>
         <Login />
         <p className="mt-5 d-block">
           Don't have an account.
         <a href="#" className="text-black" onClick={()=> {setLoginVisible(false)}}>Signup here?</a>
         </p>
-      </section>
-
-      <section className={isLoginVisible?"middleIcon middleIcon-login":"middleIcon middleIcon-signup"}>
-          <LogoSvg />
       </section>
 
       <section className={isLoginVisible?"signup-page signup-hide":"signup-page"} >
